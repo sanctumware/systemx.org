33f76a8ef1a4e43027b577832437e560953be1d7
Kevin Lin <developer@kevinlin.info>
2015-12-20 22:25:13 -0800

    Load data from disk

--- kevin-lin-main.py
+++ kevin-lin-main.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask import render_template
 
+import json
+
 app = Flask(__name__)
+data = json.load(open('data.json'))
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', data=data)
+
+
+@app.context_processor
+def replace_new_line_with_break():
+    def new_line_break(text):
+        return text.replace('\n', '<br/>')
+    return dict(new_line_break=new_line_break)
 
 
 if __name__ == '__main__':
