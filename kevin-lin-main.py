e4b6ef0c5bc0c75ea823e1912b7bb0eea7cca44b
Kevin Lin <developer@kevinlin.info>
2015-12-21 10:59:29 -0800

    Modified template and styles

--- kevin-lin-main.py
+++ kevin-lin-main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 
 import json
+import datetime
 
 app = Flask(__name__)
 data = json.load(open('data.json'))
@@ -17,6 +18,12 @@ def replace_new_line_with_break():
     def new_line_break(text):
         return text.replace('\n', '<br/>')
     return dict(new_line_break=new_line_break)
+
+
+@app.context_processor
+def current_date():
+    # Generally speaking, out of laziness to update the year every year
+    return dict(current_date=lambda: datetime.datetime.now())
 
 
 if __name__ == '__main__':
